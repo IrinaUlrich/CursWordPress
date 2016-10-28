9c39790ed2db3031fe3a64ac40fc99278b0d22a7
Irina Ulrich Jr <irinau@imprezzio.org>
2016-10-28 17:11:58 +0300

    First commit for training project

---
+++ WordPress/Class1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPress
+{
+    public class WordPressPage
+    {
+        public static int pageCount = 0;
+        public static List<WordPressPage> pendingReview = new List<WordPressPage>();
+
+        private int id;
+        public string title = "Titlu frumos";
+        private string content = "Girls are strong and independent";
+        
+
+
+        enum Status
+        {
+            Draft,
+            Published,
+            PendingReview
+        }
+
+        private Status _status;
+        private Status pageStatus 
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        private Boolean isVisible = true;
+        private DateTime publishDate = DateTime.Now;
+        private int parent;
+        private int order = 7655;
+        private int viewCount = 87777676;
+
+
+
+        public WordPressPage()
+        {
+            pageCount++;
+            id = pageCount;
+        }
+
+        public WordPressPage(string title, string content)
+        {
+            this.title = title;
+            this.content = content;
+            pageCount++;
+            id = pageCount;
+        }
+
+        public WordPressPage(string title, string content, int parent)
+        {
+            this.title = title;
+            this.content = content;
+            this.parent = parent;
+            pageCount++;
+            id = pageCount;
+        }
+
+        public bool isPublished
+        {
+            get { return pageStatus == Status.Published && publishDate <= DateTime.Now; }
+        }
+
+
+        public override string ToString()
+        {
+
+            return id.ToString() + "\n" + title + "\n" + content + "\nstatus: " + pageStatus + "\nvisible: " + isVisible + " " + publishDate + " " + parent + " " + order + " " + viewCount;
+
+        }
+
+        public void publish()
+        {
+            if (this.isPublished)
+            {
+                Console.WriteLine("from puvlish");
+                throw new SystemException("Pagina a fost deja publicata");
+
+            }
+            pageStatus = Status.Published;
+            publishDate = DateTime.Now;
+        }
+
+        public void statusPendingReview()
+        {
+            pageStatus = Status.PendingReview;
+            pendingReview.Add(this);
+        }
+
+        public void publish(int hours)
+        {
+            publish();
+            publishDate = publishDate.AddHours(hours);
+        }
+
+        public void publish(DateTime date)
+        {
+            publishDate = date;
+        }
+
+        public void setChild(WordPressPage page)
+        {
+            page.id = this.parent;
+        }
+
+        public void setParent(WordPressPage page)
+        {
+            this.parent = page.id;
+        }
+
+        public int getWordCount()
+        {
+            return content.Split(' ').Length;
+            
+        }
+
+    
+
+    }
+}
---
+++ WordPress/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPress
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            WordPressPage page1 = new WordPressPage("Titlu frumos1", "Girls are strong and independent1");
+            WordPressPage page2 = new WordPressPage("Titlu frumos2", "Girls are strong and independent1");
+            WordPressPage page3 = new WordPressPage("Titlu frumos2", "Girls are strong and independent1");
+            //           Console.WriteLine(page1);
+            //           Console.WriteLine(page2);
+            //           Console.WriteLine(page3);
+            page3.title = "NewTitlu";
+            //          Console.WriteLine(page3);
+            page3.publish();
+            //           Console.WriteLine(page3);
+            page2.statusPendingReview();
+            page3.statusPendingReview();
+//            Console.WriteLine(page3);
+//           foreach (WordPressPage p in WordPressPage.pendingReview)
+//              System.Console.WriteLine(p);
+
+            page2.publish(2);
+            //          Console.WriteLine(page2);
+            page3.publish(DateTime.Now.AddDays(2));
+            //         Console.WriteLine(page3);
+
+
+
+            WordPressPage page4 = new WordPressPage();
+
+            Console.WriteLine(page2);
+            Console.WriteLine(page3);
+            page4.setChild(page2);
+
+            page4.setParent(page3);
+
+            Console.WriteLine(page2);
+            Console.WriteLine(page3);
+            Console.WriteLine(page4);
+
+
+            Console.WriteLine(page3.getWordCount());
+            page4.publish();
+            try
+            {
+                page4.publish();
+            }
+            catch (Exception e)
+            {
+                    
+               Console.WriteLine(e.Message);
+            }
+      //      page4.publish();
+
+            Console.WriteLine(page4);
+            Console.Read();
+
+
+
+        }
+
+    }
+}
