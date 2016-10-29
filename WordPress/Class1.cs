c407ee5c02c9241e8d1030ec1dce5cd087ec93f5
Irina Ulrich <irinau@imprezzio.com>
2016-10-29 14:14:25 +0300

    Commit uni tests

--- WordPress/Class1.cs
+++ WordPress/Class1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,6 +37,7 @@ namespace WordPress
         private int parent;
         private int order = 332;
         private int viewCount = 87777676;
+
 
 
 
@@ -119,7 +121,69 @@ namespace WordPress
             return content.Split(' ').Length;
 
         }
+        // sorting started
 
+        //      public override string ToString()
+        //      {
+        //
+        //          return id.ToString() + "\n" + title + "\n" + content + "\nstatus: " + pageStatus + "\nvisible: " + isVisible + " " + publishDate + " " + parent + " " + order + " " + viewCount;
+        //
+        //      }
+
+        public static List<WordPressPage> SortByTitle(List<WordPressPage> pageList)
+        {
+            var orderedList = new List<WordPressPage>();
+         //   int indice = 0;
+        //    int countList = nameList.Count;
+        //    WordPressPage aux;
+
+
+            //ordonare dupa id
+       //     while (pageList.Count != 0)
+       //     {
+       //
+       //         var min = pageList[0];
+       //         for (int i = 0; i < pageList.Count; i++)
+       //         {
+       //             if (min.id > pageList[i].id)
+       //             {
+       //                 min = pageList[i];
+       //             }
+       //         }
+       //         pageList.Remove(min);
+       //         orderedList.Add(min);
+       //     }
+
+            while (pageList.Count > 0)
+            {
+                var selected = pageList[0];
+
+                for (int i = 1; i < pageList.Count; i++)
+                {
+                    if (selected.title.CompareTo(pageList[i].title)>0)
+                    {
+                        selected = pageList[i];
+                    }
+                }
+                pageList.Remove(selected);
+                orderedList.Add(selected);
+            }
+
+       //     while (nameList.Count != 0)
+       //     {
+       //         var min = nameList[0];
+       //         nameList.Remove(nameList[0]);
+       //         foreach (var page in nameList)
+       //         {
+       //             if ()
+       //             {
+       //                 
+       //             }
+       //         }
+       //     }
+
+            return orderedList;
+        }
 
 
     }
---
+++ WordPress/Test/UnitTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+
+namespace WordPress.Test
+{
+    [TestFixture]
+
+    public class UnitTests
+    {
+        [Test]
+        public void TestSort()
+        {
+            
+            var nameList = new List<WordPressPage>();
+            var nameListValidation = new List<WordPressPage>();
+
+            WordPressPage pageTest1 = new WordPressPage("Titlu frumos1", "Girls are strong and independent1");
+            WordPressPage pageTest2 = new WordPressPage("Titlu frumos2", "Girls are strong and independent1");
+            WordPressPage pageTest3 = new WordPressPage("Titlu frumos3", "Girls are strong and independent1");
+
+            nameList.Add(pageTest1);
+            nameList.Add(pageTest3);
+            nameList.Add(pageTest2);
+
+            nameListValidation.Add(pageTest1);
+            nameListValidation.Add(pageTest2);
+            nameListValidation.Add(pageTest3);
+
+            nameList = WordPressPage.SortByTitle(nameList);
+
+            Assert.AreEqual(nameList, nameListValidation);
+
+            Assert.AreEqual(nameList[0], pageTest1);
+            Assert.AreEqual(nameList[1], pageTest2);
+            Assert.AreEqual(nameList[2], pageTest3);
+
+        }
+    }
+}
