c4b0c4939d2cb178509d1c513a8d1b8ba6f96986
Irina Ulrich <irinau@imprezzio.com>
2016-10-29 16:42:42 +0300

    commit class wpComment

---
+++ WordPress/IPublish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPress
+{
+    public interface IPublish
+    {
+        bool isPublished { get; }
+
+        void publish();
+        void unpublish();
+    }
+}
---
+++ WordPress/WordPressComment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPress
+{
+    public class WordPressComment : IPublish
+    {
+        private bool _isPublished;
+
+
+        public bool isPublished { get; }
+        public void publish()
+        {
+            _isPublished = true;
+        }
+
+        public void unpublish()
+        {
+            _isPublished = false;
+        }
+    }
+}
